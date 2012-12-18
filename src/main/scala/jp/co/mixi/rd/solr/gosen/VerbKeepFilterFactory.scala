0d68213f7808ee4eff6cef4a0ec5e76cef49fa6b
HARUYAMA Seigo <haruyama@unixuser.org>
2012-12-18 19:50:28 +0900

    version 0.2.0 For solr 4(release) and lucene-gosen 4.0.0

--- src/main/scala/jp/co/mixi/rd/solr/gosen/VerbKeepFilterFactory.scala
+++ src/main/scala/jp/co/mixi/rd/solr/gosen/VerbKeepFilterFactory.scala
@@ -2,17 +2,11 @@ package jp.co.mixi.rd.solr.gosen
 
 import org.apache.lucene.analysis.TokenStream
 
-import org.apache.solr.analysis.BaseTokenFilterFactory
-import org.apache.solr.common.ResourceLoader
-import org.apache.solr.util.plugin.ResourceLoaderAware
-
+import org.apache.lucene.analysis.util.TokenFilterFactory
 import jp.co.mixi.rd.lucene.gosen.VerbKeepFilter
 
 
-class VerbKeepFilterFactory extends BaseTokenFilterFactory with ResourceLoaderAware {
-
-  override def inform(loader : ResourceLoader) {
-  }
+class VerbKeepFilterFactory extends TokenFilterFactory {
 
   override def create(stream: TokenStream) : TokenStream = {
     new VerbKeepFilter(stream)
--- src/test/scala/jp/co/mixi/rd/lucene/gosen/VerbKeepFilterSuite.scala
+++ src/test/scala/jp/co/mixi/rd/lucene/gosen/VerbKeepFilterSuite.scala
@@ -3,7 +3,7 @@ package jp.co.mixi.rd.lucene.gosen
 import java.io.Reader
 import java.io.StringReader
 
-import org.apache.lucene.analysis.ja.JapaneseTokenizer
+import org.apache.lucene.analysis.gosen.GosenTokenizer
 import org.apache.lucene.analysis.TokenStream
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute
 
@@ -11,38 +11,38 @@ class VerbKeepFilterSuite extends FilterSuite {
 
   test("基本1") {
     val reader = new StringReader("私は走っている")
-    val stream = new VerbKeepFilter(new JapaneseTokenizer(reader))
+    val stream = new VerbKeepFilter(new GosenTokenizer(reader))
 
     assertTokenStreamContents(stream, Array("走る", "いる"))
   }
 
   test("基本2 basicFormが '*' の場合") {
     val reader = new StringReader("はさみを用いる")
-    val stream = new VerbKeepFilter(new JapaneseTokenizer(reader))
+    val stream = new VerbKeepFilter(new GosenTokenizer(reader))
     assertTokenStreamContents(stream, Array("用いる"))
   }
 
   test("サ変1") {
     val reader = new StringReader("相談する")
-    val stream = new VerbKeepFilter(new JapaneseTokenizer(reader))
+    val stream = new VerbKeepFilter(new GosenTokenizer(reader))
     assertTokenStreamContents(stream, Array("相談する"))
   }
 
   test("サ変2") {
     val reader = new StringReader("相談した")
-    val stream = new VerbKeepFilter(new JapaneseTokenizer(reader))
+    val stream = new VerbKeepFilter(new GosenTokenizer(reader))
     assertTokenStreamContents(stream, Array("相談する"))
   }
 
   test("サ変3") {
     val reader = new StringReader("大きくした")
-    val stream = new VerbKeepFilter(new JapaneseTokenizer(reader))
+    val stream = new VerbKeepFilter(new GosenTokenizer(reader))
     assertTokenStreamContents(stream, Array("大きくする"))
   }
 
   test("組合せ1") {
     val reader = new StringReader("相談したいことがあるのですが")
-    val stream = new VerbKeepFilter(new JapaneseTokenizer(reader))
+    val stream = new VerbKeepFilter(new GosenTokenizer(reader))
     assertTokenStreamContents(stream, Array("相談する", "ある"))
   }
 }
