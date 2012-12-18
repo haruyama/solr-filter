5d8530b10b95bc87bc106d72559e308558e6c9da
HARUYAMA Seigo <haruyama@unixuser.org>
2012-12-18 20:10:43 +0900

    bug fix

--- src/main/scala/jp/co/mixi/rd/solr/gosen/YetAnotherJapanesePartOfSpeechKeepFilterFactory.scala
+++ src/main/scala/jp/co/mixi/rd/solr/gosen/YetAnotherJapanesePartOfSpeechKeepFilterFactory.scala
@@ -4,10 +4,16 @@ import org.apache.lucene.analysis.TokenStream
 
 import org.apache.lucene.analysis.util.TokenFilterFactory
 import jp.co.mixi.rd.lucene.gosen.YetAnotherJapanesePartOfSpeechKeepFilter
+import org.apache.lucene.analysis.util.ResourceLoaderAware
+import org.apache.lucene.analysis.util.ResourceLoader
 
 
 class YetAnotherJapanesePartOfSpeechKeepFilterFactory extends TokenFilterFactory {
   private var partOfSpeech = ""
+
+  def inform(loader: ResourceLoader) {
+    partOfSpeech = args.get("partOfSpeech");
+  }
 
   override def create(stream: TokenStream) : TokenStream = {
     new YetAnotherJapanesePartOfSpeechKeepFilter(stream, partOfSpeech)
