70aeeb2cc3b82c29819fad562caefef5e1d63fc3
HARUYAMA Seigo <haruyama@unixuser.org>
2013-05-07 09:45:26 +0900

    comply with Solr 4.3.0

--- src/main/scala/jp/co/mixi/rd/solr/gosen/VerbKeepFilterFactory.scala
+++ src/main/scala/jp/co/mixi/rd/solr/gosen/VerbKeepFilterFactory.scala
@@ -6,7 +6,7 @@ import org.apache.lucene.analysis.util.TokenFilterFactory
 import jp.co.mixi.rd.lucene.gosen.VerbKeepFilter
 
 
-class VerbKeepFilterFactory extends TokenFilterFactory {
+class VerbKeepFilterFactory(args : java.util.Map[String,String]) extends TokenFilterFactory(args) {
 
   override def create(stream: TokenStream) : TokenStream = {
     new VerbKeepFilter(stream)
